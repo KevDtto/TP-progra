f5ec2f0a834ffa873b77fee636428061306b73d4
KevDtto <104948198+KevDtto@users.noreply.github.com>
2022-05-22 14:57:15 -0300

    Update Mikasa.java

--- Mikasa.java
+++ Mikasa.java
@@ -13,21 +13,32 @@ public class Mikasa {
 	private double radio;
 	private double angulo;
 	private double distancia;
+	boolean convertir = false;
 	private Image image = Herramientas.cargarImagen("mikasa1.png");
+	private Image image2 = Herramientas.cargarImagen("mikasaTitan.png");
 
-	public Mikasa(double x, double y, double radio, double angulo) {
+	public Mikasa(double x, double y, double radio, double angulo, boolean convertir) {
 		this.x = x;
 		this.y = y;
 		this.radio = radio;
 		this.angulo = angulo;
+		this.convertir = convertir;
 	}
 
-	public void dibujar(Entorno e) {
-		e.dibujarCirculo(this.x, this.y, this.radio, Color.ORANGE);
-	}
-	
+//	public void dibujar(Entorno e) {
+//		e.dibujarCirculo(this.x, this.y, this.radio, Color.ORANGE);
+//	}
+
+//	public void dibujar1(Entorno e) {
+//		e.dibujarImagen(image, this.x, this.y, this.angulo, 0.1);
+//	}
+
 	public void dibujar2(Entorno e) {
-		e.dibujarImagen(image, this.x, this.y, this.angulo, 0.1);
+		if (this.convertir) {
+			e.dibujarImagen(this.image2, x, y, angulo,0.1);
+		} else {
+			e.dibujarImagen(image, this.x, this.y, this.angulo, 0.1);
+		}
 	}
 
 	// movimiento
@@ -37,16 +48,16 @@ public class Mikasa {
 			this.y += Math.sin(this.angulo) * 2;
 		}
 		if (this.x <= 20) {
-			this.x = this.x + 0.5;
+			this.x += 0.5;
 		}
 		if ((this.x) >= 800) {
-			this.x = this.x - 5;
+			this.x -= 5;
 		}
 		if (this.y >= 580) {
-			this.y = this.y - 0.5;
+			this.y -= 0.5;
 		}
 		if ((this.y) <= 0) {
-			this.y = this.y + 5;
+			this.y += 5;
 		}
 	}
 
@@ -67,7 +78,6 @@ public class Mikasa {
 	public void distancia(double x1, double y1, double x2, double y2) {
 		this.distancia = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 	}
-
 
 //	getters and setters
 	public double getX() {
@@ -110,4 +120,27 @@ public class Mikasa {
 		this.distancia = distancia;
 	}
 
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	public boolean isConvertir() {
+		return convertir;
+	}
+
+	public void setConvertir(boolean convertir) {
+		this.convertir = convertir;
+	}
+
+	public Image getImage2() {
+		return image2;
+	}
+
+	public void setImage2(Image image2) {
+		this.image2 = image2;
+	}
 }
