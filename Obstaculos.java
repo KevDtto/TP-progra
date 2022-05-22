ce8c188d1de43669d65ce4443c61c8973f5f7df3
KevDtto <104948198+KevDtto@users.noreply.github.com>
2022-05-22 14:57:54 -0300

    Update Obstaculos.java

--- Obstaculos.java
+++ Obstaculos.java
@@ -12,26 +12,19 @@ public class Obstaculos {
 		if (this.obstaculos2[0] == null) {
 			this.obstaculos2[0] = new Obstaculos2(220, 200, 120);
 		}
-		if(this.obstaculos2[1] == null){
+		if (this.obstaculos2[1] == null) {
 			this.obstaculos2[1] = new Obstaculos2(220, 450, 120);
 		}
-		if(this.obstaculos2[2] == null){
+		if (this.obstaculos2[2] == null) {
 			this.obstaculos2[2] = new Obstaculos2(560, 200, 120);
 		}
-//		if(this.obstaculos2[3] == null){
-//			this.obstaculos2[3] = new Obstaculos2(400, 400, 140);
-//		}
-		if(this.obstaculos2[3] == null){
+		if (this.obstaculos2[3] == null) {
 			this.obstaculos2[3] = new Obstaculos2(560, 450, 120);
 		}
 //		if(this.obstaculos2[4] == null){
 //			this.obstaculos2[4] = new Obstaculos2(700, 500, 140);
 //		}
-//		for (int i = 0; i < obstaculos2.length; i++) {
-//			if (this.obstaculos2[i] == null) {
-//				this.obstaculos2[i] = new Obstaculos2(Math.random() *(100-700)+700, Math.random() * (50-500)+500, 100);
-//			}
-//		}
+
 	}
 
 	public boolean colisiona(double radio1, double radio2, double dist) {
