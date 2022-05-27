6cb01560531b2d9995a0b890c08239f484f9ecc0
KevDtto <104948198+KevDtto@users.noreply.github.com>
2022-05-27 16:46:02 -0300

    Update Mikasa.java

--- Mikasa.java
+++ Mikasa.java
@@ -1,11 +1,10 @@
 package juego;
 
-import java.awt.Color;
-import java.awt.Image;
-import java.awt.Rectangle;
 
+import java.awt.Image;
 import entorno.Entorno;
 import entorno.Herramientas;
+import utils.Lista;
 
 public class Mikasa {
 	private double x;
@@ -14,6 +13,9 @@ public class Mikasa {
 	private double angulo;
 	private double distancia;
 	boolean convertir = false;
+	private Lista<Disparo> disparos;
+	private int ultimoDisparo;
+	private int sentido = 0;
 	private Image image = Herramientas.cargarImagen("mikasa1.png");
 	private Image image2 = Herramientas.cargarImagen("mikasaTitan.png");
 
@@ -23,56 +25,98 @@ public class Mikasa {
 		this.radio = radio;
 		this.angulo = angulo;
 		this.convertir = convertir;
+		this.disparos = new Lista<Disparo>();
 	}
 
 //	public void dibujar(Entorno e) {
 //		e.dibujarCirculo(this.x, this.y, this.radio, Color.ORANGE);
 //	}
 
-//	public void dibujar1(Entorno e) {
-//		e.dibujarImagen(image, this.x, this.y, this.angulo, 0.1);
-//	}
-
-	public void dibujar2(Entorno e) {
+	public void dibujarSprite(Entorno e) {
 		if (this.convertir) {
-			e.dibujarImagen(this.image2, x, y, angulo,0.1);
+			e.dibujarImagen(this.image2, x + 5, y, angulo, 0.1);
 		} else {
 			e.dibujarImagen(image, this.x, this.y, this.angulo, 0.1);
 		}
 	}
 
+	public void disparar(int contadorTicks) {
+		this.disparos.agregarAtras(new Disparo(this.x, this.y, this.angulo));
+		this.ultimoDisparo = contadorTicks;
+	}
+
 	// movimiento
 	public void moverAdelante() {
+		this.sentido = 0;
 		if (this.x > 20 && ((this.x) < 820) && this.y < 590 && ((this.y) > -30)) {
 			this.x += Math.cos(this.angulo) * 2;
 			this.y += Math.sin(this.angulo) * 2;
 		}
-		if (this.x <= 20) {
+		if (this.x <= 20 && !this.convertir) {
 			this.x += 0.5;
+		} else if (this.x <= 35 && this.convertir) {
+			this.x += 5;
 		}
-		if ((this.x) >= 800) {
+		if ((this.x) >= 785 && !this.convertir) {
+			this.x -= 5;
+		} else if (this.x >= 755 && this.convertir) {
 			this.x -= 5;
 		}
-		if (this.y >= 580) {
-			this.y -= 0.5;
+		if (this.y >= 580 && !this.convertir) {
+			this.y -= 5;
+		} else if (this.y >= 575 && this.convertir) {
+			this.y -= 5;
 		}
-		if ((this.y) <= 0) {
+		if ((this.y) <= 20 && !this.convertir) {
+			this.y += 5;
+		} else if (this.y <= 35 && this.convertir) {
 			this.y += 5;
 		}
 	}
 
 	public void moverDerecha() {
-		this.angulo += 0.08;
+		if (this.sentido == 0) {
+			this.angulo += 0.08;
+		} else {
+			this.angulo -= 0.08;
+		}
+
 	}
 
 	public void moverIzquierda() {
-		this.angulo -= 0.08;
+		if (this.sentido == 0) {
+			this.angulo -= 0.08;
+		} else {
+			this.angulo += 0.08;
+		}
 	}
 
 	public void moverAtras() {
-		this.x -= Math.cos(this.angulo) * 2;
-		this.y -= Math.sin(this.angulo) * 2;
-
+		this.sentido = 1;
+		if (this.x > 15 && ((this.x) < 820) && this.y < 590 && ((this.y) > -30)) {
+			this.x -= Math.cos(this.angulo) * 2;
+			this.y -= Math.sin(this.angulo) * 2;
+		}
+		if (this.x <= 15 && !this.convertir) {
+			this.x += 5;
+		} else if (this.x <= 30 && this.convertir) {
+			this.x += 5;
+		}
+		if ((this.x) >= 785 && !this.convertir) {
+			this.x -= 5;
+		} else if (this.x >= 755 && this.convertir) {
+			this.x -= 5;
+		}
+		if (this.y >= 580 && !this.convertir) {
+			this.y -= 5;
+		} else if (this.y >= 575 && this.convertir) {
+			this.y -= 5;
+		}
+		if ((this.y) <= 20 && !this.convertir) {
+			this.y += 5;
+		} else if (this.y <= 35 && this.convertir) {
+			this.y += 5;
+		}
 	}
 
 	public void distancia(double x1, double y1, double x2, double y2) {
@@ -142,5 +186,17 @@ public class Mikasa {
 
 	public void setImage2(Image image2) {
 		this.image2 = image2;
+	}
+
+	public int getUltimoDisparo() {
+		return this.ultimoDisparo;
+	}
+
+	public Lista<Disparo> getDisparos() {
+		return this.disparos;
+	}
+
+	public void setRayos(Lista<Disparo> disparos) {
+		this.disparos = disparos;
 	}
 }
